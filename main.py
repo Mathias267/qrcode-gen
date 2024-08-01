17b70854911afdac2c7379fcf1d3381583e9bc52
Mathias267 <mathiasmoreiraisrael@gmail.com>
2024-08-01 12:24:18 -0300

    concluído

---
+++ main.py
@@ -0,0 +1,46 @@
+from tkinter import *
+import qrcode 
+from PIL import Image, ImageDraw 
+from tkinter import messagebox
+
+def gerar_qrcode():
+    url = website_entry.get()
+
+    if len(url) == 0:
+        messagebox.showinfo(
+        title = 'Erro!',
+        message = 'Favor inserir uma URL válida...'
+        )
+    else:
+        opcao_escolhida = messagebox.askokcancel(
+            title= url,
+            message= f'O enderenço URL é:\n'
+                f'Enderenço: {url}\n'
+                f'Pronto para salvar?'               
+        )
+
+        if opcao_escolhida:
+            qr = qrcode.QRCode(kVersion=1, box_size=10, border=5)
+            qr.add_data(url)
+            qr.make(fit=True) 
+            img = qr.make_image(fill_color='red', back_color='white')
+            img.save('qrcode.png')
+
+if __name__=='__main__':
+    window = Tk()
+    window.title('Gerador de QR Code')
+    window.config(padx=10, pady=100)
+    #Labels
+    website_label = Label(text='URL: ')
+    website_label.grid(row=2, column=0)
+
+    #Entries
+    website_entry = Entry(width=35)
+    website_entry.grid(row=2, column=1, columnspan=2)
+    website_entry.focus()
+
+    # Buttons
+    add_button = Button(text='Gerar Qr Code', width=36, command=gerar_qrcode)
+    add_button.grid(row=4, column=1, columnspan=2)
+
+    window.mainloop()
